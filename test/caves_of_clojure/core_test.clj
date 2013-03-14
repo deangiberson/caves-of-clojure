27b036f1a8141c6097e873f0460131d36f0cca8d
Dean Giberson <dean@deangiberson.com>
2013-03-13 23:00:04 -0700

    http://stevelosh.com/blog/2012/07/caves-of-clojure-02/

--- test/caves_of_clojure/core_test.clj
+++ test/caves_of_clojure/core_test.clj
@@ -1,3 +1,20 @@
 (ns caves_of_clojure.core-test
   (:use clojure.test
-        caves.core))
+        caves_of_clojure.core)
+  (:import [caves_of_clojure.core UI World Game]))
+
+(defn current-ui [game]
+  (:kind (last (:uis game))))
+
+(deftest test-start
+  (let [game (new Game nil [(new UI :start)] nil)]
+
+    (testing "Enter wins at the starting screen."
+      (let [result (process-input game :enter)]
+        (is (= (current-ui result) :win))))
+
+    (testing "Other keys lose at the starting screen."
+      (let [results (map (partial process-input game)
+                         [\space \a \A :escape :up :backspace])]
+        (doseq [result results]
+          (is (= (current-ui result) :lose)))))))
