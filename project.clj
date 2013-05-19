012272eede15a3d15d5baf0c717914770ad42e1d
Dean Giberson <dean@deangiberson.com>
2013-05-18 21:29:41 -0700

    Update graphics library
    This fixes a rendering bug in Unbuntu.

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://stevelosh.com/blog/2012/07/caves-of-clojure-01/"
   :license {:name "MIT/X11"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clojure-lanterna "0.9.0"]]
+                 [clojure-lanterna "0.9.3"]]
   :main caves_of_clojure.core)
