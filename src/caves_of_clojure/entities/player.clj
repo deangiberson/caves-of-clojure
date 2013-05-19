131a0de30cb8f5af90603fa0cd33f3f78cb2980a
Dean Giberson <dean@deangiberson.com>
2013-05-18 21:59:24 -0700

    05 - Multiple entities
    http://stevelosh.com/blog/2012/07/caves-of-clojure-05/

--- src/caves_of_clojure/entities/player.clj
+++ src/caves_of_clojure/entities/player.clj
@@ -20,7 +20,7 @@
 (extend-type Player Mobile
              (move [this world dest]
                {:pre [(can-move? this world dest)]}
-               (assoc-in world [:player :location] dest))
+               (assoc-in world [:entities :player :location] dest))
              (can-move? [this world dest]
                (check-tile world dest #{:floor})))
 
@@ -35,7 +35,7 @@
   (->Player :player "@" (find-empty-tile world)))
 
 (defn move-player [world dir]
-  (let [player (:player world)
+  (let [player (get-in world [:entities :player])
         target (destination-coords (:location player) dir)]
     (cond
      (can-move? player world target) (move player world target)
--- src/caves_of_clojure/ui/drawing.clj
+++ src/caves_of_clojure/ui/drawing.clj
@@ -65,14 +65,15 @@
 
 (defn draw-hud [screen game start-x start-y]
   (let [hud-row (dec (second screen-size))
-        [x y] (get-in game [:world :player :location])
+        [x y] (get-in game [:world :entities :player :location])
         info (str "loc: [" x "-" y "]")
         info (str info " start: [" start-x "-" start-y "]")]
     (s/put-string screen 0 hud-row info)))
 
 (defmethod draw-ui :play [ui game screen]
   (let [world (:world game)
-        {:keys [tiles player]} world
+        {:keys [tiles entities]} world
+        {:keys [player]} entities
         [cols rows] screen-size
         vcols cols
         vrows (dec rows)
